9ab5f660afaac88a8e965f591327569eab7cb0a2
Matheus Valença <matheusniklaus@gmail.com>
2022-05-12 17:05:52 -0300

    criando função para excluir uma tarefa

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,12 +4,15 @@ function addCard(elemento){
   const text = prompt('Qual a tarefa?')
   const ulId = elemento.previousElementSibling.id 
   const board = document.getElementById(ulId)
-  console.log(board)
   const template = `
   <li>
     <p>${text}</p>
-    <p class="remove">x</p>
+    <p class="remove" onclick="removeCard(this)">x</p>
   </li>`
 
   board.innerHTML+=template
+}
+
+function removeCard(elemento){
+  elemento.parentNode.remove()
 }
